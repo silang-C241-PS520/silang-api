0ec2c1718a8000b54f2623ba62a2436b3ca4760e
Trunsta <81701076+Trunsta@users.noreply.github.com>
2024-06-02 20:55:20 +0700

    Setup database configuration

---
+++ utils.py
@@ -0,0 +1,11 @@
+from database import SessionLocal
+from passlib.context import CryptContext
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
