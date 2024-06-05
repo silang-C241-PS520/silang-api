f7ac8fcd3910ffbea15cdedcdda600eb1f865ecf
Trunsta <81701076+Trunsta@users.noreply.github.com>
2024-06-05 20:57:29 +0700

    Restructure project

--- authentication/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from database import Base
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True)
-    username = Column(String(35), unique=True, index=True)
-    password = Column(String)
---
+++ src/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+from database import engine
+from models import auth_models
+from routers import auth_routers
+
+tags_metadata = [
+    {
+        "name": "Authentication",
+        "description": "Operations related to the authentication and authorization of users.",
+    },
+]
+
+auth_models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Silang", openapi_tags=tags_metadata)
+
+app.include_router(auth_routers.router)
