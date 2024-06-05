6871a064a7452cf2b67c1c017e25f455960b4527
bimo.henokh <bimo.henokh@ui.ac.id>
2024-06-05 16:13:07 +0700

    create translation endpoint and docs

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
-from src.routers import auth_routers
 
 from database import engine
 from models import auth_models
-from routers import auth_routers
+from routers import auth_routers, translation_routers
 
 tags_metadata = [
     {
@@ -17,3 +16,4 @@ auth_models.Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Silang", openapi_tags=tags_metadata)
 
 app.include_router(auth_routers.router)
+app.include_router(translation_routers.router)
---
+++ src/routers/translation_routers.py
@@ -0,0 +1,75 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Response, status, UploadFile, Body
+from ..schemas.translation_schemas import TranslationRead, FeedbackUpdate
+
+router = APIRouter(
+    prefix="/api/v1/translation",
+    tags=["Translation"],
+)
+
+
+@router.get(
+    "/",
+    response_model=list[TranslationRead],
+    responses={
+        200: {"description": "Get all translation"},
+    }
+)
+async def get_all_translation():
+    # TODO
+    return [TranslationRead(id=1, video_url="video_url", translation_text="translation_text", date_created="", feedback="feedback")]
+
+
+@router.get(
+    "/{id}",
+    response_model=TranslationRead,
+    responses={
+        200: {"description": "Get translation by id"},
+        404: {"description": "Translation not found"}
+    }
+)
+async def get_translation_by_id(id: int):
+    # TODO
+    return TranslationRead(id=1, video_url="video_url", translation_text="translation_text", date_created="", feedback="feedback")
+
+
+@router.post(
+    "/",
+    status_code=201,
+    response_model=TranslationRead,
+    responses={
+        201: {"description": "Translation created"},
+        413: {"description": "Request Entity Too Large"},
+        415: {"description": "Unsupported Media Type"}
+    }
+)
+async def create_translation(file: UploadFile):
+    return TranslationRead(id=1, video_url="video_url", translation_text="translation_text", date_created="", feedback="feedback")
+
+
+@router.get(
+    "/{id}/feedbacks",
+    response_model=TranslationRead,
+    responses={
+        200: {"description": "Get feedback by id"},
+        404: {"description": "Translation not found"}
+    }
+)
+async def get_feedback_by_id(id: int):
+    # TODO
+    return TranslationRead(id=1, video_url="video_url", translation_text="translation_text", date_created="", feedback="feedback")
+
+
+@router.put(
+    "/{id}/feedbacks",
+    response_model=TranslationRead,
+    responses={
+        200: {"description": "Feedback updated"},
+        404: {"description": "Translation not found"}
+    }
+)
+async def update_feedback_by_id(id: int, feedback: FeedbackUpdate):
+    # TODO
+    return TranslationRead(id=1, video_url="video_url", translation_text="translation_text", date_created="", feedback="feedback")
+
