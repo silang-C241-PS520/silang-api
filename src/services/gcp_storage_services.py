1924247192986e00a3c67830cb57ee80d13b951c
bimo.henokh <bimo.henokh@ui.ac.id>
2024-06-11 00:37:48 +0700

    configure upload video gcp storage service

---
+++ src/services/gcp_storage_services.py
@@ -0,0 +1,23 @@
+import os
+import uuid
+from os.path import dirname, abspath
+
+from fastapi import UploadFile
+from google.cloud import storage
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = dirname(
+    dirname(dirname(abspath(__file__)))) + r'\storage-key.json'  # TODO: ini juga jadiin variable plus atur gmn pathnya
+
+
+class GCPStorageServices:
+    def __init__(self):
+        self.storage_client = storage.Client()
+        self.bucket_name = "silang-c241-ps520-dev"  # TODO: jadiin variable
+
+    def upload_file(self, file: UploadFile) -> str:
+        bucket = self.storage_client.bucket(self.bucket_name)
+        randomize_file_name = uuid.uuid4().hex
+        file_path = f"videos/{randomize_file_name}"
+        blob = bucket.blob(file_path)
+        blob.upload_from_file(file.file, content_type=file.content_type)  # TODO content_types nya apa? mp4? ato ngikutin file yg diupload?
+        return f"https://storage.googleapis.com/{self.bucket_name}/{file_path}"
