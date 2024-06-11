e0390e9e17d20c1da7cece0c99821130446daef8
Trunsta <81701076+Trunsta@users.noreply.github.com>
2024-06-11 14:26:34 +0700

    Change env variable to DB_URL

--- src/database.py
+++ src/database.py
@@ -3,13 +3,11 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DB_PASSWORD = os.environ.get("DB_PASSWORD")
-if not DB_PASSWORD :
-    print("DB_PASSWORD env variable is not set.")
+DB_URL = os.environ.get("DB_URL")
+if not DB_URL:
+    print("DB_URL env variable is not set.")
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://silang-db-prod_owner:{DB_PASSWORD}@ep-frosty-grass-a1dij1jt.ap-southeast-1.aws.neon.tech/silang-db-prod?sslmode=require"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(DB_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
