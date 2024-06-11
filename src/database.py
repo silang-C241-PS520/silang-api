a811f0301094d0d3c780d98505bfe55a5cd7a551
Trunsta <81701076+Trunsta@users.noreply.github.com>
2024-06-11 18:13:24 +0700

    Fix DB_URL typo

--- src/database.py
+++ src/database.py
@@ -3,7 +3,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DB_URL = os.environ.get("DB_PROD_URL")
+DB_URL = os.environ.get("DB_URL")
 if not DB_URL :
     print("DB_URL env variable is not set.")
 
